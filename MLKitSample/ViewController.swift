be7a71271c74e70947e501362bbde45409c61fe2
Kei Fujikawa <fujikei22funk@gmail.com>
2018-07-16 17:54:14 +0900

    show face bounding box

--- MLKitSample/ViewController.swift
+++ MLKitSample/ViewController.swift
@@ -21,18 +21,27 @@ class ViewController: UIViewController {
     // Face
     let shapeLayer = CAShapeLayer()
     
-    lazy var vision = Vision.vision()
+    private var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    private lazy var vision = Vision.vision()
+    
+    private lazy var annotationOverlayView: UIView = {
+        precondition(isViewLoaded)
+        let annotationOverlayView = UIView(frame: .zero)
+        annotationOverlayView.translatesAutoresizingMaskIntoConstraints = false
+        return annotationOverlayView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // カメラの設定
+        setupCamera()
+        setUpAnnotationOverlayView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // カメラの設定
-        setupCamera()
     }
     
     override func viewDidLayoutSubviews() {
@@ -67,6 +76,22 @@ class ViewController: UIViewController {
         camera = nil
     }
     
+    private func setUpAnnotationOverlayView() {
+        previewView.addSubview(annotationOverlayView)
+        NSLayoutConstraint.activate([
+            annotationOverlayView.topAnchor.constraint(equalTo: previewView.topAnchor),
+            annotationOverlayView.leadingAnchor.constraint(equalTo: previewView.leadingAnchor),
+            annotationOverlayView.trailingAnchor.constraint(equalTo: previewView.trailingAnchor),
+            annotationOverlayView.bottomAnchor.constraint(equalTo: previewView.bottomAnchor),
+            ])
+    }
+    
+    private func removeDetectionAnnotations() {
+        for annotationView in annotationOverlayView.subviews {
+            annotationView.removeFromSuperview()
+        }
+    }
+    
     private func setupCamera(){
         captureSession = AVCaptureSession()
         
@@ -90,10 +115,8 @@ class ViewController: UIViewController {
         
         // アウトプット
         let output = AVCaptureVideoDataOutput()
-        output.videoSettings = [
-            String(kCVPixelBufferPixelFormatTypeKey) : Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)
-        ]
-        output.alwaysDiscardsLateVideoFrames = true
+        output.videoSettings =
+            [(kCVPixelBufferPixelFormatTypeKey as String): kCVPixelFormatType_32BGRA]
         
         if captureSession.canAddOutput(output) {
             captureSession.addOutput(output)
@@ -104,12 +127,190 @@ class ViewController: UIViewController {
         output.setSampleBufferDelegate(self, queue: queue)
         
         // セッションからプレビューを表示
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.frame = view.bounds
         previewLayer.videoGravity = .resizeAspectFill
-        self.previewView.layer.addSublayer(previewLayer)
+        previewView.layer.addSublayer(previewLayer)
         
         captureSession.startRunning()
+    }
+    
+    // MARK: On-Device Detection
+    
+    private func detectFacesOnDevice(in image: VisionImage, width: CGFloat, height: CGFloat) {
+        let options = VisionFaceDetectorOptions()
+        options.landmarkType = .all
+        options.isTrackingEnabled = true
+        let faceDetector = vision.faceDetector(options: options)
+        faceDetector.detect(in: image) { features, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard error == nil, let features = features, !features.isEmpty else {
+                self.removeDetectionAnnotations()
+                print("On-Device face detector returned no results.")
+                return
+            }
+            self.removeDetectionAnnotations()
+            for face in features {
+                let normalizedRect = CGRect(
+                    x: face.frame.origin.x / width,
+                    y: face.frame.origin.y / height,
+                    width: face.frame.size.width / width,
+                    height: face.frame.size.height / height
+                )
+                let standardizedRect =
+                    self.previewLayer.layerRectConverted(fromMetadataOutputRect: normalizedRect).standardized
+                UIUtilities.addRectangle(
+                    standardizedRect,
+                    to: self.annotationOverlayView,
+                    color: UIColor.green
+                )
+                //self.addLandmarks(forFace: face, transform: self.transformMatrix(face.frame.size))
+            }
+        }
+    }
+    
+    private func transformMatrix(_ faceSize: CGSize) -> CGAffineTransform {
+        let imageViewWidth = previewView.frame.size.width
+        let imageViewHeight = previewView.frame.size.height
+        let imageWidth = previewLayer.frame.size.width
+        let imageHeight = previewLayer.frame.size.height
+        
+        let imageViewAspectRatio = imageViewWidth / imageViewHeight
+        let imageAspectRatio = imageWidth / imageHeight
+        let scale = (imageViewAspectRatio > imageAspectRatio) ?
+            imageViewHeight / imageHeight :
+            imageViewWidth / imageWidth
+        
+        // Image view's `contentMode` is `scaleAspectFit`, which scales the image to fit the size of the
+        // image view by maintaining the aspect ratio. Multiple by `scale` to get image's original size.
+        let scaledImageWidth = imageWidth * scale
+        let scaledImageHeight = imageHeight * scale
+        let xValue = (imageViewWidth - scaledImageWidth) / CGFloat(2.0)
+        let yValue = (imageViewHeight - scaledImageHeight) / CGFloat(2.0)
+        
+        var transform = CGAffineTransform.identity.translatedBy(x: xValue, y: yValue)
+        transform = transform.scaledBy(x: scale, y: scale)
+        return transform
+    }
+    
+    private func addLandmarks(forFace face: VisionFace, transform: CGAffineTransform) {
+        // Mouth
+        if let bottomMouthLandmark = face.landmark(ofType: .mouthBottom) {
+            let landmarkPoint = landmarkPointFrom(bottomMouthLandmark.position)
+            let transformedPoint = landmarkPoint.applying(transform)
+            UIUtilities.addCircle(
+                atPoint: landmarkPoint,
+                to: annotationOverlayView,
+                color: UIColor.red,
+                radius: Constants.smallDotRadius
+            )
+        }
+        if let leftMouthLandmark = face.landmark(ofType: .mouthLeft) {
+            let landmarkPoint = landmarkPointFrom(leftMouthLandmark.position)
+            let transformedPoint = landmarkPoint.applying(transform)
+            UIUtilities.addCircle(
+                atPoint: landmarkPoint,
+                to: annotationOverlayView,
+                color: UIColor.red,
+                radius: Constants.smallDotRadius
+            )
+        }
+        if let rightMouthLandmark = face.landmark(ofType: .mouthRight) {
+            let landmarkPoint = landmarkPointFrom(rightMouthLandmark.position)
+            let transformedPoint = landmarkPoint.applying(transform)
+            UIUtilities.addCircle(
+                atPoint: landmarkPoint,
+                to: annotationOverlayView,
+                color: UIColor.red,
+                radius: Constants.smallDotRadius
+            )
+        }
+        
+        // Nose
+        if let noseBaseLandmark = face.landmark(ofType: .noseBase) {
+            let landmarkPoint = landmarkPointFrom(noseBaseLandmark.position)
+            let transformedPoint = landmarkPoint.applying(transform)
+            UIUtilities.addCircle(
+                atPoint: landmarkPoint,
+                to: annotationOverlayView,
+                color: UIColor.yellow,
+                radius: Constants.smallDotRadius
+            )
+        }
+        
+        // Eyes
+        if let leftEyeLandmark = face.landmark(ofType: .leftEye) {
+            let landmarkPoint = landmarkPointFrom(leftEyeLandmark.position)
+            let transformedPoint = landmarkPoint.applying(transform)
+            UIUtilities.addCircle(
+                atPoint: landmarkPoint,
+                to: annotationOverlayView,
+                color: UIColor.cyan,
+                radius: Constants.largeDotRadius
+            )
+        }
+        if let rightEyeLandmark = face.landmark(ofType: .rightEye) {
+            let landmarkPoint = landmarkPointFrom(rightEyeLandmark.position)
+            let transformedPoint = landmarkPoint.applying(transform)
+            UIUtilities.addCircle(
+                atPoint: landmarkPoint,
+                to: annotationOverlayView,
+                color: UIColor.cyan,
+                radius: Constants.largeDotRadius
+            )
+        }
+        
+        // Ears
+        if let leftEarLandmark = face.landmark(ofType: .leftEar) {
+            let landmarkPoint = landmarkPointFrom(leftEarLandmark.position)
+            let transformedPoint = landmarkPoint.applying(transform)
+            UIUtilities.addCircle(
+                atPoint: landmarkPoint,
+                to: annotationOverlayView,
+                color: UIColor.purple,
+                radius: Constants.largeDotRadius
+            )
+        }
+        if let rightEarLandmark = face.landmark(ofType: .rightEar) {
+            let landmarkPoint = landmarkPointFrom(rightEarLandmark.position)
+            let transformedPoint = landmarkPoint.applying(transform)
+            UIUtilities.addCircle(
+                atPoint: landmarkPoint,
+                to: annotationOverlayView,
+                color: UIColor.purple,
+                radius: Constants.largeDotRadius
+            )
+        }
+        
+        // Cheeks
+        if let leftCheekLandmark = face.landmark(ofType: .leftCheek) {
+            let landmarkPoint = landmarkPointFrom(leftCheekLandmark.position)
+            let transformedPoint = landmarkPoint.applying(transform)
+            UIUtilities.addCircle(
+                atPoint: landmarkPoint,
+                to: annotationOverlayView,
+                color: UIColor.orange,
+                radius: Constants.largeDotRadius
+            )
+        }
+        if let rightCheekLandmark = face.landmark(ofType: .rightCheek) {
+            let landmarkPoint = landmarkPointFrom(rightCheekLandmark.position)
+            let transformedPoint = landmarkPoint.applying(transform)
+            UIUtilities.addCircle(
+                atPoint: landmarkPoint,
+                to: annotationOverlayView,
+                color: UIColor.orange,
+                radius: Constants.largeDotRadius
+            )
+        }
+    }
+    
+    private func landmarkPointFrom(_ visionPoint: VisionPoint) -> CGPoint {
+        return CGPoint(x: CGFloat(visionPoint.x.floatValue), y: CGFloat(visionPoint.y.floatValue))
     }
 }
 
@@ -117,15 +318,17 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         
-        let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
-        
-        let attachments = CMCopyDictionaryOfAttachments(kCFAllocatorDefault, sampleBuffer, kCMAttachmentMode_ShouldPropagate)
-        let ciImage = CIImage(cvImageBuffer: pixelBuffer!, options: attachments as! [String : Any]?)
-        
-        //leftMirrored for front camera
-        let ciImageWithOrientation = ciImage.oriented(forExifOrientation: Int32(UIImageOrientation.leftMirrored.rawValue))
-        
-        //detectFace(on: ciImageWithOrientation)
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            print("Failed to get image buffer from sample buffer.")
+            return
+        }
+        let visionImage = VisionImage(buffer: sampleBuffer)
+        let metadata = VisionImageMetadata()
+        let visionOrientation = UIUtilities.visionImageOrientation(from: UIUtilities.imageOrientation(fromDevicePosition: .front))
+        metadata.orientation = visionOrientation
+        visionImage.metadata = metadata
+        let imageWidth = CGFloat(CVPixelBufferGetWidth(imageBuffer))
+        let imageHeight = CGFloat(CVPixelBufferGetHeight(imageBuffer))
+        detectFacesOnDevice(in: visionImage, width: imageWidth, height: imageHeight)
     }
-    
 }
