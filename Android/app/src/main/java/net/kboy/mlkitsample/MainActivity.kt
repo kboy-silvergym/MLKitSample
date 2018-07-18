413d9ae516abf21e1198bd056298947397ce499e
Kei Fujikawa <fujikei22funk@gmail.com>
2018-07-18 19:13:44 +0900

    changed to android.hardware.camera2

--- Android/app/src/main/java/net/kboy/mlkitsample/MainActivity.kt
+++ Android/app/src/main/java/net/kboy/mlkitsample/MainActivity.kt
@@ -1,58 +1,216 @@
 package net.kboy.mlkitsample
 
-import android.hardware.Camera
-import android.hardware.camera2.CameraDevice
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.ImageFormat
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.*
+import android.media.ImageReader
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.support.annotation.RequiresApi
 import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import java.util.jar.Manifest
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import java.util.*
+
 
 class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    private lateinit var textureView: TextureView
+    private var captureSession: CameraCaptureSession? = null
+    private var cameraDevice: CameraDevice? = null
+    private val previewSize: Size = Size(300, 300) // FIXME:
+    private lateinit var previewRequestBuilder: CaptureRequest.Builder
+    private var imageReader: ImageReader? = null
+    private lateinit var previewRequest: CaptureRequest
+    private var backgroundThread: HandlerThread? = null
+    private var backgroundHandler: Handler? = null
 
-        if (requestCode.toString() == android.Manifest.permission.CAMERA) {
-
-        }
-    }
-
-    private var surfaceView: SurfaceView? = null
-    private var camera: Camera? = null
-    private val callBack = object: SurfaceHolder.Callback {
-        override fun surfaceCreated(holder: SurfaceHolder?) {
-            camera = Camera.open()
-
-            try {
-                camera?.setPreviewDisplay(holder)
-            } catch (e: Exception){
-                e.printStackTrace()
-            }
-        }
-
-        override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
-            camera?.startPreview()
-        }
-
-        override fun surfaceDestroyed(holder: SurfaceHolder?) {
-            camera?.release()
-            camera = null
-        }
-    }
+    private var state = 0
+    private val STATE_PREVIEW = 0
+    private val STATE_WAITING_LOCK = 1
+    private val STATE_WAITING_PRECAPTURE = 2
+    private val STATE_WAITING_NON_PRECAPTURE = 3
+    private val STATE_PICTURE_TAKEN = 4
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textureView = findViewById(R.id.mySurfaceView)
+        textureView.surfaceTextureListener = surfaceTextureListener;
+        startBackgroundThread()
+    }
 
-        ActivityCompat.requestPermissions(this,
-                arrayOf(android.Manifest.permission.CAMERA), 0)
+    fun openCamera() {
+        var manager: CameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
-        surfaceView = findViewById(R.id.mySurfaceView)
+        try {
+            var camerId: String = manager.getCameraIdList()[0]
 
-        val holder = surfaceView?.holder
-        holder?.addCallback(callBack)
+            val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                requestCameraPermission()
+                return
+            }
+
+            manager.openCamera(camerId, stateCallback, null);
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private val stateCallback = object : CameraDevice.StateCallback() {
+
+        override fun onOpened(cameraDevice: CameraDevice) {
+            //cameraOpenCloseLock.release()
+            this@MainActivity.cameraDevice = cameraDevice
+            createCameraPreviewSession()
+        }
+
+        override fun onDisconnected(cameraDevice: CameraDevice) {
+            //cameraOpenCloseLock.release()
+            cameraDevice.close()
+            this@MainActivity.cameraDevice = null
+        }
+
+        override fun onError(cameraDevice: CameraDevice, error: Int) {
+            onDisconnected(cameraDevice)
+            finish()
+        }
+
+    }
+
+    private fun createCameraPreviewSession() {
+        try {
+            val texture = textureView.surfaceTexture
+
+            // We configure the size of default buffer to be the size of camera preview we want.
+            texture.setDefaultBufferSize(previewSize.width, previewSize.height)
+
+            // This is the output Surface we need to start preview.
+            val surface = Surface(texture)
+
+            // We set up a CaptureRequest.Builder with the output Surface.
+            previewRequestBuilder = cameraDevice!!.createCaptureRequest(
+                    CameraDevice.TEMPLATE_PREVIEW
+            )
+            previewRequestBuilder.addTarget(surface)
+
+            // Here, we create a CameraCaptureSession for camera preview.
+            cameraDevice?.createCaptureSession(Arrays.asList(surface, imageReader?.surface),
+                    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+                    object : CameraCaptureSession.StateCallback() {
+
+                        override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
+                            // The camera is already closed
+                            if (cameraDevice == null) return
+
+                            // When the session is ready, we start displaying the preview.
+                            captureSession = cameraCaptureSession
+                            try {
+                                // Auto focus should be continuous for camera preview.
+                                previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
+                                        CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
+                                // Flash is automatically enabled when necessary.
+                                //setAutoFlash(previewRequestBuilder)
+
+                                // Finally, we start displaying the camera preview.
+                                previewRequest = previewRequestBuilder.build()
+                                captureSession?.setRepeatingRequest(previewRequest,
+                                        captureCallback, Handler(backgroundThread?.looper))
+                            } catch (e: CameraAccessException) {
+                                Log.e("erfs", e.toString())
+                            }
+
+                        }
+
+                        override fun onConfigureFailed(session: CameraCaptureSession) {
+                            //Tools.makeToast(baseContext, "Failed")
+                        }
+                    }, null)
+        } catch (e: CameraAccessException) {
+            Log.e("erf", e.toString())
+        }
+
+    }
+
+    private val captureCallback = object : CameraCaptureSession.CaptureCallback() {
+
+        private fun process(result: CaptureResult) {
+            when (state) {
+                STATE_PREVIEW -> Unit // Do nothing when the camera preview is working normally.
+                STATE_WAITING_LOCK -> Unit//capturePicture(result)
+                STATE_WAITING_PRECAPTURE -> {
+                    // CONTROL_AE_STATE can be null on some devices
+                    val aeState = result.get(CaptureResult.CONTROL_AE_STATE)
+                    if (aeState == null ||
+                            aeState == CaptureResult.CONTROL_AE_STATE_PRECAPTURE ||
+                            aeState == CaptureRequest.CONTROL_AE_STATE_FLASH_REQUIRED) {
+                        state = STATE_WAITING_NON_PRECAPTURE
+                    }
+                }
+                STATE_WAITING_NON_PRECAPTURE -> {
+                    // CONTROL_AE_STATE can be null on some devices
+                    val aeState = result.get(CaptureResult.CONTROL_AE_STATE)
+                    if (aeState == null || aeState != CaptureResult.CONTROL_AE_STATE_PRECAPTURE) {
+                        state = STATE_PICTURE_TAKEN
+                        //captureStillPicture()
+                    }
+                }
+            }
+        }
+    }
+
+    fun requestCameraPermission() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            AlertDialog.Builder(baseContext)
+                    .setMessage("Permission Here")
+                    .setPositiveButton(android.R.string.ok) { _, _ ->
+                        requestPermissions(arrayOf(Manifest.permission.CAMERA),
+                                200)
+                    }
+                    .setNegativeButton(android.R.string.cancel) { _, _ ->
+                        finish()
+                    }
+                    .create()
+        } else {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), 200)
+        }
+    }
+
+    private fun startBackgroundThread() {
+        backgroundThread = HandlerThread("CameraBackground").also { it.start() }
+        backgroundHandler = Handler(backgroundThread?.looper)
+    }
+
+    private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+
+        override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
+            imageReader = ImageReader.newInstance(width, height,
+                    ImageFormat.JPEG, /*maxImages*/ 2);
+
+            openCamera()
+        }
+
+        override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture?, p1: Int, p2: Int) {
+
+        }
+
+        override fun onSurfaceTextureUpdated(p0: SurfaceTexture?) {
+
+        }
+
+        override fun onSurfaceTextureDestroyed(p0: SurfaceTexture?): Boolean {
+            return false
+        }
     }
 }
