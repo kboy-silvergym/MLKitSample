3967ff58e3025089c812960d0a4b4548662636f0
Kei Fujikawa <fujikei22funk@gmail.com>
2018-07-19 15:45:58 +0900

    WIP: MLKit FaceDetection

--- Android/app/src/main/java/net/kboy/mlkitsample/MainActivity.kt
+++ Android/app/src/main/java/net/kboy/mlkitsample/MainActivity.kt
@@ -7,19 +7,19 @@ import android.graphics.ImageFormat
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.*
 import android.media.ImageReader
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
-import android.support.annotation.RequiresApi
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.util.Size
 import android.view.Surface
 import android.view.TextureView
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
 import java.util.*
 
 class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsResultCallback {
@@ -41,24 +41,53 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
         startBackgroundThread()
     }
 
-    fun openCamera() {
+    private fun openCamera() {
         var manager: CameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
         try {
-            var camerId: String = manager.getCameraIdList()[0]
+            val cameraId: String = manager.cameraIdList[0]
             val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
 
             if (permission != PackageManager.PERMISSION_GRANTED) {
                 requestCameraPermission()
                 return
             }
-            manager.openCamera(camerId, stateCallback, null);
+            manager.openCamera(cameraId, deviceStateCallback, null);
         } catch (e: Exception) {
             e.printStackTrace()
         }
     }
 
-    private val stateCallback = object : CameraDevice.StateCallback() {
+    private fun createCameraPreviewSession() {
+        try {
+            val texture = textureView.surfaceTexture
+            texture.setDefaultBufferSize(previewSize.width, previewSize.height)
+            val surface = Surface(texture)
+            previewRequestBuilder = cameraDevice!!.createCaptureRequest(
+                    CameraDevice.TEMPLATE_PREVIEW
+            )
+            previewRequestBuilder.addTarget(surface)
+            cameraDevice?.createCaptureSession(Arrays.asList(surface, imageReader?.surface),
+                    captureStateCallback, null)
+        } catch (e: CameraAccessException) {
+            Log.e("erf", e.toString())
+        }
+
+    }
+
+    private fun requestCameraPermission() {
+        requestPermissions(arrayOf(Manifest.permission.CAMERA),
+                200)
+    }
+
+    private fun startBackgroundThread() {
+        backgroundThread = HandlerThread("CameraBackground").also { it.start() }
+        backgroundHandler = Handler(backgroundThread?.looper)
+    }
+
+    // MARK : - Callbacks -------------------
+
+    private val deviceStateCallback = object : CameraDevice.StateCallback() {
 
         override fun onOpened(cameraDevice: CameraDevice) {
             this@MainActivity.cameraDevice = cameraDevice
@@ -77,60 +106,11 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
 
     }
 
-    private fun createCameraPreviewSession() {
-        try {
-            val texture = textureView.surfaceTexture
-            texture.setDefaultBufferSize(previewSize.width, previewSize.height)
-            val surface = Surface(texture)
-            previewRequestBuilder = cameraDevice!!.createCaptureRequest(
-                    CameraDevice.TEMPLATE_PREVIEW
-            )
-            previewRequestBuilder.addTarget(surface)
-            cameraDevice?.createCaptureSession(Arrays.asList(surface, imageReader?.surface),
-                    object : CameraCaptureSession.StateCallback() {
-
-                        override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
-
-                            if (cameraDevice == null) return
-                            captureSession = cameraCaptureSession
-                            try {
-                                previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
-                                        CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
-                                previewRequest = previewRequestBuilder.build()
-                                captureSession?.setRepeatingRequest(previewRequest,
-                                        null, Handler(backgroundThread?.looper))
-                            } catch (e: CameraAccessException) {
-                                Log.e("erfs", e.toString())
-                            }
-
-                        }
-
-                        override fun onConfigureFailed(session: CameraCaptureSession) {
-                            //Tools.makeToast(baseContext, "Failed")
-                        }
-                    }, null)
-        } catch (e: CameraAccessException) {
-            Log.e("erf", e.toString())
-        }
-
-    }
-
-    fun requestCameraPermission() {
-        requestPermissions(arrayOf(Manifest.permission.CAMERA),
-                200)
-    }
-
-    private fun startBackgroundThread() {
-        backgroundThread = HandlerThread("CameraBackground").also { it.start() }
-        backgroundHandler = Handler(backgroundThread?.looper)
-    }
-
     private val surfaceTextureListener = object : TextureView.SurfaceTextureListener {
 
         override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
-            imageReader = ImageReader.newInstance(width, height,
-                    ImageFormat.JPEG, /*maxImages*/ 2);
-
+            imageReader = ImageReader.newInstance(width, height, ImageFormat.JPEG, /*maxImages*/ 2);
+            imageReader?.setOnImageAvailableListener(mOnImageAvailableListener, backgroundHandler)
             openCamera()
         }
 
@@ -146,4 +126,54 @@ class MainActivity : AppCompatActivity(), ActivityCompat.OnRequestPermissionsRes
             return false
         }
     }
+
+    private val captureStateCallback = object : CameraCaptureSession.StateCallback() {
+        override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
+
+            if (cameraDevice == null) return
+            captureSession = cameraCaptureSession
+            try {
+                previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
+                        CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
+                previewRequest = previewRequestBuilder.build()
+                captureSession?.setRepeatingRequest(previewRequest,
+                        captureCallback, Handler(backgroundThread?.looper))
+            } catch (e: CameraAccessException) {
+                Log.e("erfs", e.toString())
+            }
+        }
+
+        override fun onConfigureFailed(session: CameraCaptureSession) {
+            //Tools.makeToast(baseContext, "Failed")
+        }
+    }
+
+    private val captureCallback = object : CameraCaptureSession.CaptureCallback() {
+
+    }
+
+    private val mOnImageAvailableListener = object : ImageReader.OnImageAvailableListener {
+
+        override fun onImageAvailable(reader: ImageReader?) {
+            val bitmap = textureView.bitmap
+            val visionImage = FirebaseVisionImage.fromBitmap(bitmap)
+            val options = FirebaseVisionFaceDetectorOptions.Builder()
+                    .setModeType(FirebaseVisionFaceDetectorOptions.FAST_MODE)
+                    .setLandmarkType(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                    .setClassificationType(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                    .setMinFaceSize(0.15f)
+                    .setTrackingEnabled(true)
+                    .build()
+            val detector = FirebaseVision.getInstance().getVisionFaceDetector(options)
+            detector.detectInImage(visionImage)
+                    .addOnSuccessListener {
+                        print("good job")
+                    }
+                    .addOnFailureListener {
+                        it.printStackTrace()
+                    }
+        }
+
+    }
+
 }
